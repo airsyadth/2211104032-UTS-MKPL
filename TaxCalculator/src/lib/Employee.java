647ae6a46a094ddb3d0b690199be10b6eda801de
Arzario Irsyad Al Fatih <rberpendidikan@gmail.com>
2025-04-17 15:52:56 +0700

    Refactor konstruktor Employee dengan mengganti parameter tanggal menjadi LocalDate
    Mengurangi panjang parameter konstruktor Employee dengan mengganti yearJoined, monthJoined, dan dayJoined menjadi satu parameter LocalDate untuk meningkatkan keterbacaan dan fleksibilitas

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -31,15 +31,15 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+	private LocalDate joinDate;
+	
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, LocalDate joinDate, boolean isForeigner, boolean isMale) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.idNumber = idNumber;
 		this.address = address;
-		this.yearJoined = yearJoined;
-		this.monthJoined = monthJoined;
-		this.dayJoined = dayJoined;
+		this.joinDate = joinDate;
 		this.isForeigner = isForeigner;
 		this.gender = gender;
 		
